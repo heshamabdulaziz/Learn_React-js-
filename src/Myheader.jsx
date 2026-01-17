8484c9b61684a37d3f735a210b384abab5d81e8c
hesham <heshamabdulaziz88@gmail.com>
2026-01-17 02:56:29 -0800

    wrap compenent items with  div or  react fragmant (<> </>)

--- src/Myheader.jsx
+++ src/Myheader.jsx
@@ -1,6 +1,14 @@
 export default function Myheader(){
+/* 
+when we have more then one item in component wen have to  
+wrap compenent items with  div or  react fragmant (<> </>)
 
+*/
 return(
+    <div>
 <h1>Header</h1>
+<h2>h2 from myheader</h2>
+
+</div>
     )
 }
