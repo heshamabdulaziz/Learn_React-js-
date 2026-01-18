8ef8ba6340c3c2fcd181978ea4d780928698adb3
hesham <heshamabdulaziz88@gmail.com>
2026-01-17 22:51:45 -0800

    anthor way to access the props  using props.

--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,10 +1,10 @@
 export default function PersonCard(props){
-let {name,age,country}=props;
+//let {name,age,country}=props;
     return(
   <div style={{backgroundColor:"red",color:"whitesmoke"}}> 
-    <h2>{name}</h2>
-    <h2>{age}</h2>
-    <h2>{country}</h2>
+    <h2>{props.name}</h2>
+    <h2>{props.age}</h2>
+    <h2>{props.country}</h2>
 
     </div>
 
