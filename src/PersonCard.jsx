d813e0c7086b3669ef5b4b5886099acae808f756
hesham <heshamabdulaziz88@gmail.com>
2026-01-17 22:55:32 -0800

    desturct  the props directly

--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,10 +1,12 @@
-export default function PersonCard(props){
+export default function PersonCard({name,age,country}){
 //let {name,age,country}=props;
+//console.log(props);
+
     return(
   <div style={{backgroundColor:"red",color:"whitesmoke"}}> 
-    <h2>{props.name}</h2>
-    <h2>{props.age}</h2>
-    <h2>{props.country}</h2>
+    <h2>{name}</h2>
+    <h2>{age}</h2>
+    <h2>{country}</h2>
 
     </div>
 
