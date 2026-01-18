e757b42d2d388d976a1cc9d45c7d852e12dd228b
hesham <heshamabdulaziz88@gmail.com>
2026-01-17 19:50:13 -0800

    merge html element with js

--- src/Myheader.jsx
+++ src/Myheader.jsx
@@ -11,10 +11,21 @@ let obj={
 
 return(
     <div>
+       {
+        // call function using JSX
+       } 
+        <h1> {jsx()}</h1>
 <h1 style={{backgroundColor:"green",color:"red"}}>Name:{obj.name}</h1>
 <h1 style={{backgroundColor:"green",color:"black"}}>Name:{obj.nationality}</h1>
 <h1 style={{backgroundColor:"green",color:"white"}}>Name:{obj.age}</h1>
 
 </div>
     )
+
+    
 }
+ 
+function jsx(){
+
+    return "hi hesham th laern JSX"
+ }
