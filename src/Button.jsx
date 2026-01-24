fa607adc929f46735891e46e17b3e47f47d39c7a
hesham <heshamabdulaziz88@gmail.com>
2026-01-24 00:31:15 -0800

    Managing Input with ( useState)

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 
 export default function Button(){
+    
     const[text,setText]=useState("Start");
-   
+    const[textInput,settextInput]=useState("");
+
+   // intail value can be  number or text or object ,..... 
 const btn=()=>{
-  setText("Stop");
+  setText("Stop");}
+  // bind html input using useState
+ function handleinput(e){
 
+ settextInput(e.target.value)
+//console.log(e);
 
+ }
 
-}
 return(
-  <button style={{backgroundColor:"green" ,color:"red", fontSize:"50px"}}
+  <div>
+      <h2 style={{color:"red"}}>bind html input using useState </h2>
+      <input type="text" value={textInput} onChange={handleinput}></input>
+      <p>{textInput}</p>
+    <h2 style={{color:"red"}}>useState hook in react </h2>
+<button style={{backgroundColor:"green" ,color:"red", fontSize:"50px"}}
     onClick={()=>{btn()}}  >{text}</button>  
-)
 
+
+  </div>
+  
+)
 }
