e95ce95bcf8cfac8766ad082689c43c6d8e6edba
hesham <heshamabdulaziz88@gmail.com>
2026-02-23 23:32:12 -0800

    fetch on post with axios

--- src/hooks/UseEffect/UseEffectWith_Fetch_Data.jsx
+++ src/hooks/UseEffect/UseEffectWith_Fetch_Data.jsx
@@ -4,8 +4,11 @@ import { useEffect, useState } from "react";
 
 const  UseEffectWith_Fetch_Data=()=>{
  const[posts,setPosts]=useState([]);
+ const[postId,setPostId]=useState(1);
+ const[post,setPost]=useState({});
 const[loading,setloading]=useState(true)
 const[error,setError]=useState(false)
+
  /*const fetchPosts=async()=>{
 const res=await axios.get("https://jsonplaceholder.typicode.com/posts");
 return setPosts(res.data);
@@ -13,7 +16,7 @@ return setPosts(res.data);
  }  */
 
  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res)=>{
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res)=>{
         setPosts(res.data)
         setloading(false)
     }).catch((err)=>{
@@ -25,9 +28,32 @@ return setPosts(res.data);
 
  },[])
 
+ //fetch one post 
+ 
+  useEffect(()=>{
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`).then((res)=>{
+        setPost(res.data)
+        setloading(false)
+    }).catch((err)=>{
+        setError(true)
+        setloading(false)
+        console.log(err.error);
+        
+    })
+
+ },[postId])
+ 
 return(
    <div>
-    <h2>Fetch data </h2>
+    <label>Enter on post Number</label>
+    <input type="number" value={postId} placeholder="Enter postNumber " onChange={(e)=>setPostId(e.target.value)}/>
+  <h2>Fetch one post </h2>
+  {
+
+    <pr>{post.id} - {post.title}</pr>
+  }
+
+    <h2>Fetch ALL posts </h2>
     { error&&(<h1>something went wrong</h1>)}
 
 
